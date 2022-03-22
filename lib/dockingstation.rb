68c07c70320bcd342ada4d11d7d9316c0a8e5d65
almorcrette <26277447+almorcrette@users.noreply.github.com>
2022-03-22 17:52:37 +0000

    Fails to be_working? test Have a bike but bike isn't working

--- lib/dockingstation.rb
+++ lib/dockingstation.rb
@@ -1,5 +1,5 @@
 class DockingStation
-  def release_bike()
-    puts "Hello free bike"
+  def release_bike
+    Bike.new
   end
 end
--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -1,11 +1,19 @@
 require 'dockingstation'
+require 'bike'
 
 describe DockingStation do
   it "responses to release_bike" do
     expect(DockingStation.new).to respond_to(:release_bike)
-    # expect(obj).to respond_to(:foo) # pass if obj.respond_to?(:foo)
   end
-  # it "creates Dockingstation instance" do
-  #   expect(Dockingstation.new.class).to eq Dockingstation
-  # end
+  
+  it "release a bike object which is working when method :release_bike is used" do
+    p DockingStation.new.release_bike
+    expect(DockingStation.new.release_bike).to be_working?
+  end
+    
+    
+  # expect(actual).to be_xxx         # passes if actual.xxx?
+# expect(actual).to have_xxx(:arg) # passes if actual.has_xxx?(:arg)
+  
+  
 end
